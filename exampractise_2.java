5ae59a32133a70b35be20287c33b831dd3fbf481
Veer-Tech Trendings <49147903+speedyj007@users.noreply.github.com>
2019-06-07 23:53:19 +0530

    The Main File

---
+++ exampractise_2.java
@@ -0,0 +1,19 @@
+
+public class exampractise_2{
+	
+	public static void main(String []args)
+	{
+		String word = "abcde";
+		
+		
+		System.out.println();
+		for(int i = 0;i<2;i++ )
+		{
+			System.out.print(word.charAt(i)+" ");
+		}
+		System.out.println();
+	
+
+		
+	}
+}
